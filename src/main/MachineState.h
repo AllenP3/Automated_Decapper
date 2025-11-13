a0ed95b0f91002a38dcacb19aea822f252735202
Mo03YO <yousefelnaggar04@gmail.com>
2025-11-13 20:52:27 +0100

    Create MachineState.h

---
+++ src/main/MachineState.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <Arduino.h>
+
+class MachineState {
+public:
+  static void init();
+
+  // global status
+  static bool running;        // a routine is currently active
+  static bool needsHoming;    // must home before next SNAP/SCREW
+
+  // counters
+  static long snapCount;      // successful SNAP cycles
+  static long screwCount;     // successful SCREW cycles
+};
