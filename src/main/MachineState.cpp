da474b18c44b7e22dada4761f4fca9d0189fd68b
Mo03YO <yousefelnaggar04@gmail.com>
2025-11-13 20:53:12 +0100

    Create MachineState.cpp

---
+++ src/main/MachineState.cpp
@@ -0,0 +1,14 @@
+#include "MachineState.h"
+
+bool MachineState::running     = false;
+bool MachineState::needsHoming = false;
+
+long MachineState::snapCount   = 0;
+long MachineState::screwCount  = 0;
+
+void MachineState::init() {
+  running     = false;
+  needsHoming = false;
+  snapCount   = 0;
+  screwCount  = 0;
+}
